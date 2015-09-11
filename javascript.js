ec4f336c45ffdb94b7915f0e08db85165d0902fd
danielmdesigns <danielwmaldonado@gmail.com>
2015-09-11 15:02:53 -0500

    initial commit
    new simplist app using MDL

---
+++ javascript.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+"use strict";
+//alert if input left blank
+$('#fab').click( function(e){
+   if($('input').val() === ""){
+      e.stopImmediatePropagation();
+      alert('Sorry, but your input can not be left blank.');
+	return false;
+   }
+});
+
+//$('#simplist').on("keypress", function(e){
+//    if(e.keyCode === 13 ){
+//        var $item = $("input").val();
+//        $("ul").append("<li>" + $item + "</li>");
+//    }
+//});
+    
+//list logic & browser storage
+//add using "button"
+$("#fab").on("click", function(){
+  var $item = $("input").val();
+  $("ul").append("<li>" + $item + "</li>");
+    
+//set
+$('#form')[0].reset();
+  var list = $('#list').html();
+  localStorage.setItem('list', list);
+  return false;
+});
+    
+//shows
+if(localStorage.getItem('list')){
+  $('#list').html(localStorage.getItem('list'));
+}
+    
+//remove
+$("ul").on("click", "li", function (){
+    $(this).remove();
+    var i = $(this).text();
+	// get the current list as a string.
+    var currentList = localStorage.getItem('list');
+	// replace the clicked item with a blank string.
+    var newList = currentList.replace('<li>' + i + '</li>', '');
+	// update the localStorage with the new list
+    localStorage.setItem('list', newList);
+});
+
+$("#goback").on("click", function(){
+	//alert("im working");
+	window.location.href = "simplist.html";
+	return false;
+});
+
+});
