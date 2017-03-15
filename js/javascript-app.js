7c6093ca3bd7b0f6808791339a6cca04a746c793
Daniel Maldonado <daniel@danielmdesigns.com>
2017-03-15 10:30:15 -0500

    :wrench: .status-bar visible in landscape
    On initial start up of the app, this fix lets us determine if we need
    to show the status bar or not…
    
    …that said, while the application is open, if the orientation is then
    handled in landscape mode, the proposed changes do not apply.
    
    Still working on a fix for this

--- js/javascript-app.js
+++ js/javascript-app.js
@@ -137,6 +137,9 @@ $("#js-ath").hide();
 	
 //IF VIEWING IN APP MODE
 if(window.navigator.standalone){
+	
+if(window.innerHeight > window.innerWidth){
+	//portrait = true;
 	var statusBarProperties = {
 		"position":"fixed",
 		"z-index":"100",
@@ -147,8 +150,14 @@ if(window.navigator.standalone){
 	};
 	$(".statusbar").css(statusBarProperties);
 	$("header").css("margin-top","1.325rem");
-	//$("img.nav-toggle.close").css("top","2.5rem");
 	$(".nav-toggle.close").css("top","2.5rem");
+}else{
+	//portrait = false; in landscape
+	$(".statusbar").css("padding","0");
+	$("header").css("margin-top","5px");
+	$(".nav-toggle.close").css("top","1rem");
+}
+	
 }else if(window.navigator.standalone === false){
 	//SHOW ATH MESSAGE EVERY 30s
 	setTimeout(function(){
